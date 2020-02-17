3bf4651e60e8385587823452a6b842b72349141d
Lee Peters <lpeters0023@gmail.com>
2020-02-16 21:45:25 -0500

    improved readability / comments
    @Habenzy

--- index_2.js
+++ index_2.js
@@ -6,70 +6,78 @@ function ask(questionText) {
     rl.question(questionText, resolve);
   });
 } 
+// | - - - starter code - do not change above - - - |
 
 let minGuess = 0
 let maxGuess = 100
-let guessCount = [] // keep track of guess count
+let guessCount = [] // array to track user guesses 
 let pluralOrnot = "try"
 
-start()
+// | - - - initialize game  - - - |
 
+start() 
 async function start() {
   console.log("Let's play a game where you (human) make up a number between 0 and 100 and I (computer) try to guess it.")
+  
   let secretNumber = parseInt(await ask("What is your secret number?\nI won't peek, I promise...\n")); 
-  console.log('You entered: ' + secretNumber);
-  // console.log(typeof(secretNumber)) testing input
-  let computerGuess = randNumGen(minGuess, maxGuess)
+    console.log('You entered: ' + secretNumber);
+
+  let computerGuess = randNumGen(minGuess, maxGuess) // assign random integer within range to variable
 
   let userAnswerYN = await ask('Is your secret number ' +computerGuess+'?') // assign user input 
-      userAnswerYN = inputConverter(userAnswerYN) // standardize input
+      userAnswerYN = inputConverter(userAnswerYN) // standardize input to trimmed lower case string
 
-  guessCount.push(computerGuess)
+  guessCount.push(computerGuess) // tracking player guesses in array
 
     if (userAnswerYN === "y") {
         console.log('I win! And I guessed in ' + guessCount.length + pluralOrnot)
-        } // if computer guesses in a single try
+        } 
 
   while (userAnswerYN !== "y") {
-      let userAnswerHL = await ask('Is it higher or lower?') // assign user input > higher or lower than guess
-          userAnswerHL = inputConverter(userAnswerHL) // standardize input
-          if (secretNumber < computerGuess && userAnswerHL === "h") {
-            console.log("incorrect input, please start again") 
-            process.exit() // guard clause checking against accidental input
-          }
-          if (secretNumber > computerGuess && userAnswerHL === "l") {
-            console.log("incorrect input, please start again") 
-            process.exit() // guard clause 
-          }
-        if (userAnswerHL === "l") {
-          maxGuess = computerGuess
-          //console.log(minGuess, maxGuess)
-          computerGuess = Math.floor((minGuess + maxGuess) / 2) 
-          guessCount.push(computerGuess) 
-        } 
-        if (userAnswerHL === "h") {
-          minGuess = computerGuess
-          //console.log(minGuess, maxGuess)    
-          computerGuess = Math.floor((minGuess + maxGuess) / 2)
-          guessCount.push(computerGuess)
-        }
+    let userAnswerHL = await ask('Is it higher or lower?') // assign user input 
+        userAnswerHL = inputConverter(userAnswerHL) // standardize input to trimmed lower case string
 
-        userAnswerYN = await ask('Is your secret number ' +computerGuess+'?') 
-        userAnswerYN = inputConverter(userAnswerYN)
+      // guard clause checking against accidental input
+      if (secretNumber < computerGuess && userAnswerHL === "h") {
+        console.log("incorrect input, please start again") 
+        process.exit() 
+      }
+      if (secretNumber > computerGuess && userAnswerHL === "l") {
+        console.log("incorrect input, please start again") 
+        process.exit() 
+      }
 
-      if (userAnswerYN === "y") {
-          pluralOrnot = " tries"
-          console.log('I win! And I guessed in ' + guessCount.length + pluralOrnot) // possible to include outside of while loop?
-          }
+    if (userAnswerHL === "l") {
+      maxGuess = computerGuess
+      computerGuess = Math.floor((minGuess + maxGuess) / 2) // re-assign computer guess based on narrowed range
+      guessCount.push(computerGuess) 
+    } 
+    if (userAnswerHL === "h") {
+      minGuess = computerGuess 
+      computerGuess = Math.floor((minGuess + maxGuess) / 2)
+      guessCount.push(computerGuess)
+    }
+
+    userAnswerYN = await ask('Is your secret number ' +computerGuess+'?') 
+    userAnswerYN = inputConverter(userAnswerYN)
+
+    if (userAnswerYN === "y") {
+       pluralOrnot = " tries"
+       console.log('I win! And I guessed in ' + guessCount.length + pluralOrnot)
+    }
   }
   process.exit()
 }
 
-function randNumGen(min, max) { // generates random number within range (exclusive on both ends)
+// | - - - process functions - do not change below  - - - |
+
+// func to return random integer (inclusive on both ends)
+function randNumGen(min, max) { 
   let randNum =  Math.floor(Math.random() * (max - min + 1)) + min 
   return randNum;
 }
-function inputConverter(string) { // standardizes input
+// func to standardize input to return a trimmed lower case string
+function inputConverter(string) { 
   string = string.toString().trim().toLowerCase();
   let convertedInput = string[0];
   return convertedInput;
